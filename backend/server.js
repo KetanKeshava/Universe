18b8a54fe28aad1d7863a12cd949e1b6f79dad98
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2023-12-08 22:35:46 -0500

    add cors

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,8 @@ app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
 
+app.use(cors());
+
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
