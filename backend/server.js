7f67b05738fc13487f6072d5f3c1cbfea66e3a28
Ketan Keshava <keshava.ke@northeastern.edu>
2023-12-08 22:45:19 -0500

    Reverting sever.js changes

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,6 @@ app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
 
-app.use(cors());
-
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
